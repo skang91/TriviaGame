e578060639ae89565ceed61ab1e6917a3ab72830
Somin Kang <somin.kang.91@gmail.com>
2020-05-06 19:17:58 -0700

    work untill 5/6

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,76 +3,70 @@
 $("#questionnaire").hide();
 $("#results-page").hide();
 
-//clicking Start Button
-$("#start-button").click(function(){
-	$("#start-button").hide();
-	$("#questionnaire").show();
+
+//Variables
+var counter = 30;
+var intervalId;
+var correctCount = 0;
+var wrongCount = 0;
+var unanswered = 0;
+
+
+//functions
+
+//for timer
+function countdownTimer(){
+    intervalId = setInterval(decrement, 1000);
+}
+function decrement(){
+    counter--;
+    $("#timer").html(" " + counter + " " + "Seconds");
+    if (counter ===1){
+        $("#timer").html(" " + counter + " " + "Second");
+    }
+    else if(counter ===0) {
+        stop();
+        hide();
+        displaySummary();
+    }
+}
+function stop() {
+    clearInterval(intervalId);
+}
+
+//hide questions after time is up
+function hide(){
+    $("#questionnaire").hide();
+}
+
+
+//Results Page
+function displaySummary(){
+    $("#results-page").show();
+    unanswered = (8-(correctCount+wrongCount));
+    $("#correct-score").text(" " + correctCount); 
+    $("#incorrect-score").text(" " + wrongCount); 
+    $("#unanswered-score").text(" " + unanswered); 
+}
+
+//Radio button
+$('input[type=radio]').change(function(){
+    correctCount = $('input[value=correct]:checked').length;
+    wrongCount = $('input[value=wrong]:checked').length;
+    unanswered = (8-(correctCount+wrongCount));
 });
 
 
-//Variables
-var counter =50;
-var intervalID;
-var correctScore = 0;
-var incorrectScore = 0;
-var unansweredScore = 0;
+//Start Button
+$("#start-button").click(function (){
+	$("#start-button").hide();
+    $("#questionnaire").show();
+    countdownTimer();
+});
 
-
-//function
-
-var countingDown = document.getElementById("timer")
-var counter = 31;
-var newElement = document.createElement("span");
-newElement.innerHTML = "30";
-var id;
-
-countingDown.parentNode.replaceChild(newElement, countingDown);
-
-id = setInterval(function() {
-    counter--;
-    if(counter === 0){
-		setTimeout(function(){
-			$("#questionnaire").hide();
-			$("#results-page").show();
-		}, 1000);
-	}
-    if(counter < 0) {
-        newElement.parentNode.replaceChild(countingDown, newElement);
-        clearInterval(id);
-    } else {
-        newElement.innerHTML = counter.toString();
-    }
-}, 1000);
-
-
-
-
-var correctScore = 0;
-var incorrectScore = 0;
-var unansweredScore = 0;
-
-
-$("#correct-score").text(correctScore);
-$("#incorrect-score").text(incorrectScore);
-$("#unanswered-score").text(unansweredScore);
-
-
-var answer = $('.q').val();
-
-
-
-if(answer === "wrong"){
-	incorrectScore++;
-} else if(answer === undefined){
-	unansweredScore++;
-} else{ 
-    if(answer === "correct"){
-    correctScore++;
-    }
-}
-
-//clinking Done Button	
-$("#done-button").click(function(){
+//Done Button	
+$("#done-button").click(function (){
 	$("#questionnaire").hide();
-	$("#results-page").show();
+    $("#results-page").show();
+    displaySummary();
 });
